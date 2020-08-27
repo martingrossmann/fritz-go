38ebdcb2f1f97e6e9191ea0fb43a34bb9598ba74
martingrossmann <github@martymaximus.de>
2020-08-27 17:37:42 +0200

    Updated influxdboutput.go with real data

--- writer/influxdboutput.go
+++ writer/influxdboutput.go
@@ -2,23 +2,25 @@ package writer
 
 import (
 	"context"
+	"github.com/influxdata/influxdb-client-go/v2"
 	"github.com/martingrossmann/fritz-go/common"
 	"github.com/martingrossmann/fritz-go/fritz"
-	"time"
-
-	"github.com/influxdata/influxdb-client-go/v2"
+	"log"
 )
 
 func WriteToInnflux(conf common.Config, counter fritz.OnlineCounter) {
+	log.Println("Trying to connect to influxdb " + conf.InfluxHost)
 	// Create a new client using an InfluxDB server base URL and an authentication token
-	client := influxdb2.NewClient("http://localhost:9999", "my-token")
+	client := influxdb2.NewClient(conf.InfluxHost, conf.InfluxUser+":"+conf.InfluxPassword)
 	// Use blocking write client for writes to desired bucket
-	writeAPI := client.WriteAPIBlocking("my-org", "my-bucket")
-	// Create point using fluent style
-	p := influxdb2.NewPointWithMeasurement("stat").
-		AddTag("unit", "temperature").
-		AddField("avg", 23.2).
-		AddField("max", 45).
-		SetTime(time.Now())
+	writeAPI := client.WriteAPIBlocking("", conf.InfluxDB)
+
+	// Create new point with counter data
+	p := influxdb2.NewPointWithMeasurement(conf.InfluxTcMeasurement).
+		AddTag("host", "fritzbox").
+		AddField(conf.InfluxTcSent, counter.DataSent).
+		AddField(conf.InfluxTcReceived, counter.DataReceived).
+		SetTime(counter.DayOfData)
 	writeAPI.WritePoint(context.Background(), p)
+	log.Println("Counter data written to influxdb successfully.")
 }
