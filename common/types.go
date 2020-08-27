aa3d850bbca2edea64eb1d8a0e435c9671a323d1
martingrossmann <github@martymaximus.de>
2020-08-27 17:35:16 +0200

    Updated configuration

--- common/types.go
+++ common/types.go
@@ -1,11 +1,16 @@
 package common
 
 type Config struct {
-	FritzHost      string `properties:"fb.host,default=http://fritz.box"`
-	FritzPassword  string `properties:"fb.password"`
-	CsvFile        string `properties:"csv.filename"`
-	InfluxHost     string `properties:"influxdb.host"`
-	InfluxDB       string `properties:"influxdb.db"`
-	InfluxUser     string `properties:"influxdb.user"`
-	InfluxPassword string `properties:"influxdb.password"`
+	FritzHost           string `properties:"fb.host,default=http://fritz.box"`
+	FritzPassword       string `properties:"fb.password"`
+	CsvActive           bool   `properties:"csv.active,default=true"`
+	CsvFile             string `properties:"csv.filename"`
+	InfluxActive        bool   `properties:"influxdb.active,default=false"`
+	InfluxHost          string `properties:"influxdb.host"`
+	InfluxDB            string `properties:"influxdb.db"`
+	InfluxUser          string `properties:"influxdb.user"`
+	InfluxPassword      string `properties:"influxdb.password"`
+	InfluxTcMeasurement string `properties:"influxdb.traffic.measurement"`
+	InfluxTcReceived    string `properties:"influxdb.traffic.received"`
+	InfluxTcSent        string `properties:"influxdb.traffic.sent"`
 }
